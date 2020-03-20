13e81851001dd6e430fedaa054d3467d7947bb7f
RJ <Argelisjiminian1@gmail.com>
2020-03-19 22:40:03 -0400

    finished

--- index.js
+++ index.js
@@ -45,8 +45,19 @@ function Person(name,age) {
   this.stomach = [];
 }
 
-Person.prototype.eat("someFood") = function(){
-  this.eat
+Person.prototype.eat = function(someFood){
+  if(this.stomach.length < 10){
+    this.stomach.push(someFood);
+  }
+
+}
+
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
 }
 
 /*
@@ -63,9 +74,21 @@ Person.prototype.eat("someFood") = function(){
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+this.model = model;
+this.milesPerGallon = milesPerGallon;
+this.tank = 0;
+this.odometer = 0;    
 }
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
+
+Car.prototype.drive = function(distance){
+  this.odometer = this.odometer + distance;
+  this.tank = this.tank - (distance / this.milesPerGallon)
+}
+
 
 /*
   TASK 3
@@ -74,15 +97,26 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+this.name = name;
+this.age = age;
+this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(playWith){
+  return `Playing with ${this.favoriteToy}`;
+}
+
+
+
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
+  1. Can be used in its own scope.
   2. 
   3. 
   4. 
